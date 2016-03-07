25444df0d0e8baba30111ca1367a1064da25f3ce
Ivo Wetzel <ivo.wetzel@googlemail.com>
2016-03-07 22:23:55 +0100

    Update docs for 0.8.0

---
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['cobalt'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='cobalt/enum.ConnectionState.html' title='cobalt::ConnectionState'>ConnectionState</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='cobalt/struct.ConnectionID.html' title='cobalt::ConnectionID'>ConnectionID</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='cobalt/enum.MessageKind.html' title='cobalt::MessageKind'>MessageKind</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='cobalt/struct.Stats.html' title='cobalt::Stats'>Stats</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
